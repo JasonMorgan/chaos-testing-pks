afd8e9716e944a7e06c83f18cdc2e893bc48d2ce
Jason Morgan <jmorgan@f9vs.com>
2019-10-06 17:51:30 -0500

    Adding readme and curl tool.

---
+++ curl-planespotter.ps1
@@ -0,0 +1,14 @@
+#!/usr/local/microsoft/powershell/6/pwsh
+
+while ($true) {
+  try {
+    $time = Measure-Command {$return = Invoke-WebRequest -Uri https://planespotter.59s.io -ErrorAction Stop}
+    Write-Output "Curled https://planespotter.59s.io, return code $($return.StatusCode), $($time.Milliseconds) milliseconds"
+} 
+  catch {
+    Write-Output "Failed to curl https://planespotter.59s.io"
+  }
+  
+  Start-Sleep -Seconds 2
+}
+	
