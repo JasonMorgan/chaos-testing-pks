74aa10beed7eba9f22e4c8d4e621abc45aa99377
Jason Morgan <jmorgan@f9vs.com>
2019-10-07 08:41:27 -0500

    changing times.

--- disrupt-pods.ps1
+++ disrupt-pods.ps1
@@ -4,8 +4,8 @@ while ($true) {
   $pods = kubectl get pods -l app=planespotter-frontend --output json | out-string | convertfrom-json
   $target = $pods.items.where{$_.status.phase -match 'running'} | Get-Random -Count 1
   Write-Output "Deleting pod $($target.metadata.name)"
-  Start-Sleep -Seconds 5
+  Start-Sleep -Seconds 10
   kubectl delete pod $target.metadata.name
   Write-Output "Waiting..."
-  Start-Sleep -Seconds 10
+  Start-Sleep -Seconds 15
 }
